42baf5b980563f0cce6176a4f29bdc8fc942600a
wxnshw <wender.ribeiro8@gmail.com>
2025-12-18 10:03:06 -0300

    First components

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./navbar/navbar.component";
+import { SidebarComponent } from "./sidebar/sidebar.component";
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, NavbarComponent, SidebarComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'BiosatFrontend';
+}
