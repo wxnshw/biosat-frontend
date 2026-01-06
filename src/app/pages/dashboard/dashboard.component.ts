095ab3ff294ab92165b2ae879c64ca2a90c14e7c
wxnshw <wender.ribeiro8@gmail.com>
2026-01-06 01:19:17 -0300

    Dashboard page customization

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,11 +1,131 @@
 import { Component } from '@angular/core';
 import { MiniAgendaComponent } from "../../components/mini-agenda/mini-agenda.component";
+import { AgendaComponent } from "../agenda/agenda.component";
+import { PieChartComponent } from '../../charts/pie-chart/pie-chart.component';
+import { LineChartComponent } from '../../charts/line-chart/line-chart.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [MiniAgendaComponent],
+  standalone: true,
+  imports: [MiniAgendaComponent, PieChartComponent, LineChartComponent, CommonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
+  pendentes = [
+    {
+      cliente: 'João Silva',
+      tipo: 'Lavagem Completa',
+      data: '2024-12-03'
+    },
+    {
+      cliente: 'Maria Souza',
+      tipo: 'Polimento',
+      data: '2024-12-04'
+    }
+  ];
+
+  concluidos = [
+    {
+      cliente: 'Carlos Lima',
+      tipo: 'Higienização',
+      data: '2024-12-01'
+    },
+    {
+      cliente: 'Ana Paula',
+      tipo: 'Lavagem Simples',
+      data: '2024-11-30'
+    },
+    {
+      cliente: 'Carlos Lima',
+      tipo: 'Higienização',
+      data: '2024-12-01'
+    },
+    {
+      cliente: 'Ana Paula',
+      tipo: 'Lavagem Simples',
+      data: '2024-11-30'
+    },
+    {
+      cliente: 'Carlos Lima',
+      tipo: 'Higienização',
+      data: '2024-12-01'
+    },
+    {
+      cliente: 'Ana Paula',
+      tipo: 'Lavagem Simples',
+      data: '2024-11-30'
+    },
+    {
+      cliente: 'Carlos Lima',
+      tipo: 'Higienização',
+      data: '2024-12-01'
+    },
+    {
+      cliente: 'Ana Paula',
+      tipo: 'Lavagem Simples',
+      data: '2024-11-30'
+    },
+    {
+      cliente: 'Carlos Lima',
+      tipo: 'Higienização',
+      data: '2024-12-01'
+    },
+    {
+      cliente: 'Ana Paula',
+      tipo: 'Lavagem Simples',
+      data: '2024-11-30'
+    },
+    {
+      cliente: 'Carlos Lima',
+      tipo: 'Higienização',
+      data: '2024-12-01'
+    },
+    {
+      cliente: 'Ana Paula',
+      tipo: 'Lavagem Simples',
+      data: '2024-11-30'
+    },
+    {
+      cliente: 'Carlos Lima',
+      tipo: 'Higienização',
+      data: '2024-12-01'
+    },
+    {
+      cliente: 'Ana Paula',
+      tipo: 'Lavagem Simples',
+      data: '2024-11-30'
+    },
+    {
+      cliente: 'Carlos Lima',
+      tipo: 'Higienização',
+      data: '2024-12-01'
+    },
+    {
+      cliente: 'Ana Paula',
+      tipo: 'Lavagem Simples',
+      data: '2024-11-30'
+    },
+    {
+      cliente: 'Carlos Lima',
+      tipo: 'Higienização',
+      data: '2024-12-01'
+    },
+    {
+      cliente: 'Ana Paula',
+      tipo: 'Lavagem Simples',
+      data: '2024-11-30'
+    },
+    {
+      cliente: 'Carlos Lima',
+      tipo: 'Higienização',
+      data: '2024-12-01'
+    },
+    {
+      cliente: 'Ana Paula',
+      tipo: 'Lavagem Simples',
+      data: '2024-11-30'
+    }
+  ];
 }
